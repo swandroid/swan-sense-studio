3df54208bc7cc105a767a1247480eaa4bd284934
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-03-07 15:42:19 +0100

    Fix improper heart rate sensor initialization

--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/HeartRateSensor.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/HeartRateSensor.java
@@ -1,12 +1,15 @@
 package interdroid.swan.sensors.impl.wear;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import interdroid.swan.R;
 import interdroid.swan.sensors.AbstractConfigurationActivity;
 import interdroid.swan.sensors.AbstractSwanSensor;
+import interdroid.swan.sensors.impl.wear.shared.RemoteSensorManager;
 
 /**
  * Created by slavik on 2/22/16.
@@ -14,6 +17,11 @@ import interdroid.swan.sensors.AbstractSwanSensor;
 public class HeartRateSensor extends AbstractSwanSensor {
 
     public static final String TAG = "HeartRateSensor";
+    public static final String HEART_RATE = "heart_rate";
+    RemoteSensorManager sensorMngr;
+
+
+    ArrayList<Integer>  ids = new ArrayList<>();
 
     public static class ConfigurationActivity extends
             AbstractConfigurationActivity {
@@ -26,23 +34,25 @@ public class HeartRateSensor extends AbstractSwanSensor {
     }
     @Override
     public void initDefaultConfiguration(Bundle defaults) {
-
+        Log.d("Heart Rate Sensor", "Init default configuration");
+        sensorMngr = RemoteSensorManager.getInstance(this);
     }
 
     @Override
     public void register(String id, String valuePath, Bundle configuration) throws IOException {
-
+        SENSOR_NAME = "Wear Heart Rate Sensor";
+        Log.d("Heart RATE", "Register");
     }
 
     @Override
     public void unregister(String id) {
-
+        Log.d("Heart RATE", "Register");
     }
 
     @Override
     public String[] getValuePaths() {
-        return new String[0];
-    }
+        return new String[]{ "heart_rate" };
+    };
 
     @Override
     public void onConnected() {
