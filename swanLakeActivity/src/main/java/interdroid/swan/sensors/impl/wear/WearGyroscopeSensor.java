097e89603c7b42edd69c5139e51754a196277f33
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-03-16 15:32:26 +0100

    Add wear gyroscope sensor

--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearGyroscopeSensor.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearGyroscopeSensor.java
@@ -1,7 +1,49 @@
 package interdroid.swan.sensors.impl.wear;
+
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import interdroid.swan.R;
+import interdroid.swan.sensors.AbstractConfigurationActivity;
+import interdroid.swan.sensors.impl.wear.shared.AbstractWearSensor;
+
 /**
  * Created by Veaceslav Munteanu on 14-March-16.
  * @email veaceslav.munteanu90@gmail.com
  */
-public class WearGyroscopeSensor {
+public class WearGyroscopeSensor extends AbstractWearSensor{
+
+    public static final String X_FIELD = "x";
+    public static final String Y_FIELD = "y";
+    public static final String Z_FIELD = "z";
+
+    protected static final int HISTORY_SIZE = 30;
+
+    public static class ConfigurationActivity extends
+            AbstractConfigurationActivity {
+
+        @Override
+        public final int getPreferencesXML() {
+            return R.xml.gyroscope_preferences;
+        }
+
+    }
+
+
+    @Override
+    public final void register(String id, String valuePath, Bundle configuration) throws IOException{
+
+        SENSOR_NAME = "Wear Gyroscope Sensor";
+        sensor_name = "Gyroscope";
+        valuePathMappings.put(X_FIELD, 0);
+        valuePathMappings.put(Y_FIELD, 1);
+        valuePathMappings.put(Z_FIELD, 2);
+
+        super.register(id, valuePath,configuration);
+    }
+    @Override
+    public String[] getValuePaths() {
+        return new String[] { X_FIELD, Y_FIELD, Z_FIELD };
+    }
 }
