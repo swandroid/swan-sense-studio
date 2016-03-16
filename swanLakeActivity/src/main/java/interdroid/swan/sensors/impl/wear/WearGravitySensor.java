0fdb6e995a2808620aeef8da987f7aa3e95daf11
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-03-16 16:35:13 +0100

    Add step counter and gravity sensor for wear

--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearGravitySensor.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearGravitySensor.java
@@ -1,8 +1,43 @@
 package interdroid.swan.sensors.impl.wear;
 
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import interdroid.swan.R;
+import interdroid.swan.sensors.AbstractConfigurationActivity;
+import interdroid.swan.sensors.impl.wear.shared.AbstractWearSensor;
+
 /**
  * Created by Veaceslav Munteanu on 14-March-16.
  * @email veaceslav.munteanu90@gmail.com
  */
-public class WearGravitySensor {
+public class WearGravitySensor extends AbstractWearSensor{
+
+    public static final String X_FIELD = "x";
+    public static final String Y_FIELD = "y";
+    public static final String Z_FIELD = "z";
+
+    public static class ConfigurationActivity extends AbstractConfigurationActivity {
+        @Override
+        public final int getPreferencesXML() {
+            return R.xml.gravity_preferences;
+        }
+    }
+
+    @Override
+    public void register(String id, String valuePath, Bundle configuration) throws IOException {
+        SENSOR_NAME = "Wear Gravity Sensor";
+        sensor_name = "Gravity";
+        valuePathMappings.put(X_FIELD, 0);
+        valuePathMappings.put(Y_FIELD, 1);
+        valuePathMappings.put(Z_FIELD, 2);
+
+        super.register(id, valuePath, configuration);
+    }
+
+    @Override
+    public String[] getValuePaths() {
+        return new String[] { X_FIELD, Y_FIELD, Z_FIELD};
+    }
 }
--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearStepCounterSensor.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearStepCounterSensor.java
@@ -1,8 +1,41 @@
 package interdroid.swan.sensors.impl.wear;
 
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import interdroid.swan.R;
+import interdroid.swan.sensors.AbstractConfigurationActivity;
+import interdroid.swan.sensors.impl.wear.shared.AbstractWearSensor;
+
 /**
  * Created by Veaceslav Munteanu on 14-March-16.
  * @email veaceslav.munteanu90@gmail.com
  */
-public class WearStepCounterSensor {
+public class WearStepCounterSensor extends AbstractWearSensor{
+    public static final String TAG = "Wear Step Counter Sensor";
+
+    /**
+     * Step counter
+     */
+    public static final String STEP_COUNTER = "step_counter";
+
+    public static class ConfigurationActivity extends AbstractConfigurationActivity {
+        @Override
+        public final int getPreferencesXML() {
+            return R.xml.stepcounter_preferences;
+        }
+    }
+
+    @Override
+    public void register(String id, String valuePath, Bundle configuration) throws IOException {
+        SENSOR_NAME = "Wear Step Counter Sensor";
+        sensor_name = "Step Counter";
+        valuePathMappings.put(STEP_COUNTER, 0);
+        super.register(id, valuePath, configuration);
+    }
+    @Override
+    public String[] getValuePaths() {
+        return new String[] { STEP_COUNTER };
+    }
 }
