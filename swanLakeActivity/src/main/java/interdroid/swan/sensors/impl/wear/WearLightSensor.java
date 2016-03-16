9e109ceb97b74db6381e3f086f9cfc4318a9466f
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-03-16 16:02:47 +0100

    Implement wear light sensor

--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearLightSensor.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearLightSensor.java
@@ -1,8 +1,53 @@
 package interdroid.swan.sensors.impl.wear;
 
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import interdroid.swan.R;
+import interdroid.swan.sensors.AbstractConfigurationActivity;
+import interdroid.swan.sensors.impl.wear.shared.AbstractWearSensor;
+
 /**
  * Created by Veaceslav Munteanu on 14-March-16.
  * @email veaceslav.munteanu90@gmail.com
  */
-public class WearLightSensor {
+public class WearLightSensor extends AbstractWearSensor {
+
+
+
+    public static final String LUX_FIELD = "lux";
+
+    /**
+     * The configuration activity for this sensor.
+     *
+     * @author nick &lt;palmer@cs.vu.nl&gt;
+     *
+     */
+    public static class ConfigurationActivity extends
+            AbstractConfigurationActivity {
+
+        @Override
+        public final int getPreferencesXML() {
+            return R.xml.light_preferences;
+        }
+
+    }
+
+    @Override
+    public String[] getValuePaths() {
+        return new String[] { LUX_FIELD };
+    }
+
+
+
+    @Override
+    public void register(String id, String valuePath, Bundle configuration) throws IOException {
+        SENSOR_NAME = "Wear Light Sensor";
+        sensor_name = "Light";
+        valuePathMappings.put(LUX_FIELD, 0);
+
+        super.register(id, valuePath, configuration);
+    }
+
 }
