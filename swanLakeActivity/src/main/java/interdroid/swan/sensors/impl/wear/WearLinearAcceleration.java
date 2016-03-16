8febd102522c93af6addb592d8adde2135792113
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-03-16 16:09:50 +0100

    Implement wear linear acceleration sensor

--- swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearLinearAcceleration.java
+++ swanLakeActivity/src/main/java/interdroid/swan/sensors/impl/wear/WearLinearAcceleration.java
@@ -1,9 +1,61 @@
 package interdroid.swan.sensors.impl.wear;
 
 
+import android.os.Bundle;
+
+import java.io.IOException;
+
+import interdroid.swan.R;
+import interdroid.swan.sensors.AbstractConfigurationActivity;
+import interdroid.swan.sensors.impl.wear.shared.AbstractWearSensor;
+
 /**
  * Created by Veaceslav Munteanu on 14-March-16.
  * @email veaceslav.munteanu90@gmail.com
  */
-public class WearLinearAcceleration {
+public class WearLinearAcceleration extends AbstractWearSensor{
+
+    public static final String X_FIELD = "x";
+    public static final String Y_FIELD = "y";
+    public static final String Z_FIELD = "z";
+    public static final String TOTAL_FIELD = "total";
+
+    public static final String TAG = "LinearAccelerationSensor";
+
+    /**
+     * The configuration activity for this sensor.
+     *
+     * @author Veaceslav Munteanu
+     *
+     */
+    public static class ConfigurationActivity extends
+            AbstractConfigurationActivity {
+
+        @Override
+        public final int getPreferencesXML() {
+            return R.xml.movement_preferences;
+        }
+
+    }
+
+    @Override
+    public void initDefaultConfiguration(Bundle defaults) {
+
+    }
+
+    @Override
+    public void register(String id, String valuePath, Bundle configuration) throws IOException {
+        SENSOR_NAME = "Wear Movement Sensor";
+        sensor_name = "Accelerometer";
+        valuePathMappings.put(X_FIELD, 0);
+        valuePathMappings.put(Y_FIELD, 1);
+        valuePathMappings.put(Z_FIELD, 2);
+
+        super.register(id, valuePath, configuration);
+    }
+
+    @Override
+    public String[] getValuePaths() {
+        return new String[] { X_FIELD, Y_FIELD, Z_FIELD, TOTAL_FIELD };
+    }
 }
