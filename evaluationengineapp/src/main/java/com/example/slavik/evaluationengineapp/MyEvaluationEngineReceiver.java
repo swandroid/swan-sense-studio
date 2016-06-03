641a2ce5e3ebc3f0aa890d01d13d7bc334e2c638
Veaceslav Munteanu <veaceslav.munteanu90@gmail.com>
2016-06-03 12:00:37 +0200

    Rename EvaluationEngineReceiver to EvaluationEngineReceiverBase

--- evaluationengineapp/src/main/java/com/example/slavik/evaluationengineapp/MyEvaluationEngineReceiver.java
+++ evaluationengineapp/src/main/java/com/example/slavik/evaluationengineapp/MyEvaluationEngineReceiver.java
@@ -3,15 +3,14 @@ package com.example.slavik.evaluationengineapp;
 import android.content.Context;
 import android.content.Intent;
 
-import interdroid.swancore.engine.EvaluationEngineReceiver;
-import interdroid.swancore.engine.EvaluationEngineServiceBase;
+import interdroid.swancore.engine.EvaluationEngineReceiverBase;
 
 /**
  * Created by Veaceslav Munteanu on 6/1/16.
  *
  * @email veaceslav.munteanu90@gmail.com
  */
-public class MyEvaluationEngineReceiver extends EvaluationEngineReceiver {
+public class MyEvaluationEngineReceiver extends EvaluationEngineReceiverBase {
     public void onReceive(Context context, Intent intent) {
         // forward the intent to the service
         intent.setClass(context, MyEvaluationEngine.class);
--- swancore/src/main/java/interdroid/swancore/engine/EvaluationEngineReceiverBase.java
+++ swancore/src/main/java/interdroid/swancore/engine/EvaluationEngineReceiverBase.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class EvaluationEngineReceiver extends BroadcastReceiver {
+public class EvaluationEngineReceiverBase extends BroadcastReceiver {
 
     /**
      * This receiver acts as a forwarder to the EvaluationEngineServiceBase.
