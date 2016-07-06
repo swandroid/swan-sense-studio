83782d2deadfe0856d776a21450826cca1b050af
Roshan Bharath Das <roshanbharathdas@gmail.com>
2016-07-06 19:00:06 +0200

    Updated cloud test activity

--- phone/src/main/java/interdroid/swan/remote/cloud/CloudTestActivity.java
+++ phone/src/main/java/interdroid/swan/remote/cloud/CloudTestActivity.java
@@ -8,10 +8,14 @@ import interdroid.swan.R;
 import interdroid.swancore.swanmain.ExpressionManager;
 import interdroid.swancore.swanmain.SensorInfo;
 import interdroid.swancore.swanmain.SwanException;
+import interdroid.swancore.swanmain.TriStateExpressionListener;
 import interdroid.swancore.swanmain.ValueExpressionListener;
+import interdroid.swancore.swansong.Expression;
 import interdroid.swancore.swansong.ExpressionFactory;
 import interdroid.swancore.swansong.ExpressionParseException;
 import interdroid.swancore.swansong.TimestampedValue;
+import interdroid.swancore.swansong.TriState;
+import interdroid.swancore.swansong.TriStateExpression;
 import interdroid.swancore.swansong.ValueExpression;
 
 /**
@@ -53,24 +57,52 @@ public class CloudTestActivity extends Activity {
     private void registerSWANSensor(String myExpression){
 
         try {
-            ExpressionManager.registerValueExpression(this, String.valueOf(REQUEST_CODE),
-                    (ValueExpression) ExpressionFactory.parse(myExpression),
-                    new ValueExpressionListener() {
+            Expression checkExpression =  ExpressionFactory.parse(myExpression);
 
-                        /* Registering a listener to process new values from the registered sensor*/
-                        @Override
-                        public void onNewValues(String id,
-                                                TimestampedValue[] arg1) {
-                            if (arg1 != null && arg1.length > 0) {
-                                String value = arg1[0].getValue().toString();
-                                tv.setText("Value = "+value);
+            if(checkExpression instanceof ValueExpression) {
 
-                            } else {
-                                tv.setText("Value = null");
+
+                ExpressionManager.registerValueExpression(this, String.valueOf(REQUEST_CODE),
+                        (ValueExpression) ExpressionFactory.parse(myExpression),
+                        new ValueExpressionListener() {
+
+                            /* Registering a listener to process new values from the registered sensor*/
+                            @Override
+                            public void onNewValues(String id,
+                                                    TimestampedValue[] arg1) {
+                                if (arg1 != null && arg1.length > 0) {
+                                    String value = arg1[0].getValue().toString();
+                                    tv.setText("Value = " + value+"\nTimestamp = "+arg1[0].getTimestamp());
+
+                                } else {
+                                    tv.setText("Value = null");
+                                }
+
                             }
+                        });
 
-                        }
-                    });
+            }
+            else if(checkExpression instanceof TriStateExpression){
+
+                ExpressionManager.registerTriStateExpression(this, String.valueOf(REQUEST_CODE),
+                        (TriStateExpression) ExpressionFactory.parse(myExpression), new TriStateExpressionListener() {
+                            @Override
+                            public void onNewState(String id, long timestamp, TriState newState) {
+
+
+                                    tv.setText("Tristate ="+newState+"\nTimestamp = "+timestamp);
+
+
+
+                            }
+                        });
+
+
+            }
+
+
+
+
         } catch (SwanException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
