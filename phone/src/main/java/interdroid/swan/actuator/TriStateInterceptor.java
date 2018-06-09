c48be1548ea36b0d7cca1f38947f46b21ca97b06
Long Tran <lngtrn94@gmail.com>
2018-06-09 21:10:33 +0200

    functional listeners

--- phone/src/main/java/interdroid/swan/actuator/TriStateInterceptor.java
+++ phone/src/main/java/interdroid/swan/actuator/TriStateInterceptor.java
@@ -15,32 +15,46 @@ public class TriStateInterceptor extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, intent.toString());
 
-        Intent forward = intent.getParcelableExtra(ActuatorManager.EXTRA_FORWARD);
+        switch (TriState.valueOf(intent.getStringExtra(ExpressionManager.EXTRA_NEW_TRISTATE))) {
+            case TRUE:
+                forwardExtraIntent(context, intent, ActuatorManager.EXTRA_FORWARD_TRUE);
+
+                String expressionId = intent.getStringExtra(ActuatorManager.EXTRA_EXPRESSION_ID);
+
+                if (expressionId == null) {
+                    Log.w(TAG, "Empty expressionId");
+                    return;
+                }
+
+                Actuator actuator = ActuationManager.ACTUATORS.get(expressionId);
+
+                if (actuator == null) {
+                    Log.w(TAG, "No actuator registered for id " + expressionId);
+                    return;
+                }
+
+                Log.d(TAG, "Performing actuator for id " + expressionId);
+                actuator.performAction();
+                break;
+            case FALSE:
+                forwardExtraIntent(context, intent, ActuatorManager.EXTRA_FORWARD_FALSE);
+                break;
+            case UNDEFINED:
+                forwardExtraIntent(context, intent, ActuatorManager.EXTRA_FORWARD_UNDEFINED);
+                break;
+        }
+    }
+
+    private void forwardExtraIntent(Context context, Intent intent, String extra) {
+        Intent forward = intent.getParcelableExtra(extra);
         if (forward != null) {
+            forward.putExtra(ExpressionManager.EXTRA_NEW_TRISTATE,
+                    intent.getStringExtra(ExpressionManager.EXTRA_NEW_TRISTATE));
+            forward.putExtra(ExpressionManager.EXTRA_NEW_TRISTATE_TIMESTAMP,
+                    intent.getLongExtra(ExpressionManager.EXTRA_NEW_TRISTATE_TIMESTAMP, 0));
+
             context.sendBroadcast(forward);
         }
-
-        if (TriState.TRUE != TriState.valueOf(intent.getStringExtra(ExpressionManager.EXTRA_NEW_TRISTATE))) {
-            return;
-        }
-
-        String expressionId = intent.getStringExtra(ActuatorManager.EXTRA_EXPRESSION_ID);
-
-        if (expressionId == null) {
-            Log.w(TAG, "Empty expressionId");
-            return;
-        }
-
-        Actuator actuator = ActuationManager.ACTUATORS.get(expressionId);
-
-        if (actuator == null) {
-            Log.w(TAG, "No actuator registered for id " + expressionId);
-            return;
-        }
-
-        Log.d(TAG, "Performing actuator for id " + expressionId);
-        actuator.performAction();
     }
 }
